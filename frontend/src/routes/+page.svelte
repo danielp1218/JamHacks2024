73c81fe041a68ffa40e7f497996a3f48a769ea8f
danielp1218 <danielpu2007@gmail.com>
2024-06-07 23:02:10 -0400

    init

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Button } from 'svelte-ux';
+</script>
+
+<Button>Click here</Button>
+
+<h1 class="text-6xl">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
